eb8eb0c5f481661fb72e6ea8f51709a505883f05
Dhananjaya Poojari <DPoojari@magsw.com>
2021-02-23 18:22:54 +0530

    For netlify deploy

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 var cors = require('cors')
+const serverless = require("serverless-http");
 
 const db=require('./DB/connection')
 db();
@@ -9,8 +10,9 @@ app.use(cors())
 app.use(express.json())
     
 const task=require('./routers/TaskApis')
-app.use('/task',task)
+app.use('/.netlify/functions/server',task)
 
 
 
-app.listen(4000,()=>console.log('Server started'))
+module.exports = app;
+module.exports.handler = serverless(app);
