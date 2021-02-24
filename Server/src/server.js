4e858b1b1c94101dc100a202fc1b2d7e0fa48d24
Dhananjaya Poojari <DPoojari@magsw.com>
2021-02-24 09:33:59 +0530

    cc

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-var cors = require('cors')
+//var cors = require('cors')
 const serverless = require("serverless-http");
 
 const db=require('./DB/connection')
 db();
 
-app.use(cors())
+//app.use(cors())
 //app.use(express.json())
     
 const task=require('./routers/TaskApis')
