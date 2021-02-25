0b87151cf71c4ab680db703d2b40b40165d82f99
Dhananjaya Poojari <DPoojari@magsw.com>
2021-02-25 09:57:48 +0530

    New API

---
+++ Server/src/task.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+var cors = require('cors')
+const serverless = require("serverless-http");
+
+const db=require('./DB/connection')
+db();
+
+app.use(cors())
+app.use(express.json())
+    
+const task=require('./routers/TaskApis')
+app.use('/.netlify/functions/task',task)
+
+
+
+module.exports = app;
+module.exports.handler = serverless(app);
