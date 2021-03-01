c11453ac16d94445a35f5459113cfcb9bd2f4b00
Dhananjaya <dhananjaya-developer>
2021-03-01 08:54:03 +0530

    kk

--- Web/src/components/Login.jsx
+++ Web/src/components/Login.jsx
@@ -1,15 +1,39 @@
-import {useState} from "react";
-import {sessionGet,sessionSet} from '../sessionStorage'
+import { useState } from "react";
+import { sessionGet, sessionSet } from "../sessionStorage";
+
+const checkSession = async () => {
+  let sessio_id = sessionGet("user_token");
+  if(!sessio_id)
+    return false;
+  const res = await fetch(
+    `https://tasktrackerserver.netlify.app/.netlify/functions/server/session`,
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ token: sessio_id }),
+    }
+  );
+  const data = await res.json();
+  if (data.username) return true;
+  else return false;
+};
 
 export default function Login() {
-  let sessio_id=sessionGet('user_token')
-
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  let data=checkSession();
+  if(data.username){
+    window.location='/task';
+    return;
+  }
+
   const login = async () => {
-    var user={}
-    user.username=username;
-    user.password=password;
+    var user = {};
+    user.username = username;
+    user.password = password;
     const res = await fetch(
       `https://tasktrackerserver.netlify.app/.netlify/functions/server/login`,
       {
@@ -20,10 +44,10 @@ export default function Login() {
         body: JSON.stringify(user),
       }
     );
-    const data=await res.json();
-    if(data.token){
-      sessionSet('user_token',data.token);
-      window.location='/task'
+    const data = await res.json();
+    if (data.token) {
+      sessionSet("user_token", data.token);
+      window.location = "/task";
     }
   };
   return (
@@ -34,38 +58,38 @@ export default function Login() {
       <h1>Please Log In</h1>
       <div className="container row">
         <div className="jumbotron col-sm-4 pull-center">
-            <div className="form-group">
-              <label>Username:</label>
-              <input
-                className="form-control"
-                required
-                type="text"
-                name="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-            <div className="form-group">
-              <label>Password:</label>
-              <input
-                className="form-control"
-                required
-                type="password"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <div className="form-group">
-              {/* <Link to="/task"> */}
-                <input
-                  className="btn btn-primary"
-                  type="submit"
-                  value="Log In"
-                  onClick={login}
-                />
-              {/* </Link> */}
-            </div>
+          <div className="form-group">
+            <label>Username:</label>
+            <input
+              className="form-control"
+              required
+              type="text"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label>Password:</label>
+            <input
+              className="form-control"
+              required
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            {/* <Link to="/task"> */}
+            <input
+              className="btn btn-primary"
+              type="submit"
+              value="Log In"
+              onClick={login}
+            />
+            {/* </Link> */}
+          </div>
         </div>
       </div>
     </div>
